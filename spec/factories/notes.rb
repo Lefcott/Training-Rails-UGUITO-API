93f01c6bdcfc6c4839c228edc004bc96ede7d0f2
Leandro Cotti <leandro.cotti@widergy.com>
2024-02-20 11:11:44 -0300

    fix: use values instead of keys

--- spec/factories/notes.rb
+++ spec/factories/notes.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :note do
     title { Faker::Book.title }
     content { Faker::Lorem.paragraphs(number: 3).join("\n") }
-    type { Note.types.keys.sample }
+    type { Note.types.values.sample }
     user
   end
 end
