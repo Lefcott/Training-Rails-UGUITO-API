2fe58dd7272fe83c8c8a1bc729aff0caac62972e
Leandro Cotti <leandro.cotti@widergy.com>
2024-02-20 11:05:20 -0300

    fix: sample type from model instead of hardcoding a number

--- spec/factories/notes.rb
+++ spec/factories/notes.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :note do
     title { Faker::Book.title }
     content { Faker::Lorem.paragraphs(number: 3).join("\n") }
-    type { 1 }
+    type { Note.types.keys.sample }
     user
   end
 end
