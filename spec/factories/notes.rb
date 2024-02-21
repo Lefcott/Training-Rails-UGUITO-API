bbbc6b45f4f46a6ac6f322f3b8d851b8b73beb0c
Leandro Cotti <leandro.cotti@widergy.com>
2024-02-21 19:40:30 -0300

    fix: remove comment

--- spec/factories/notes.rb
+++ spec/factories/notes.rb
@@ -5,6 +5,5 @@ FactoryBot.define do
     content { Faker::Lorem.paragraphs(number: 3).join("\n") }
     type { Note.types.values.sample }
     utility
-    # association :user, factory: :user
   end
 end
