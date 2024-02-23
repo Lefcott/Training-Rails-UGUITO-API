3780433f2cfd3633ced57b9727c057c98d48880d
Leandro Cotti <leandro.cotti@widergy.com>
2024-02-23 12:28:20 -0300

    feat: tests for GET #index

---
+++ spec/controllers/api/v1/notes_controller_spec.rb
@@ -0,0 +1,60 @@
+require 'rails_helper'
+
+describe Api::V1::NotesController, type: :controller do
+  describe 'GET #index' do
+    let(:user) { create(:user) }
+    let(:user_notes) { create_list(:note, 5, user: user) }
+
+    let!(:expected) do
+      ActiveModel::Serializer::CollectionSerializer.new(notes_expected,
+                                                        serializer: IndexNoteSerializer).to_json
+    end
+
+    context 'when fetching all the notes for user' do
+      let(:notes_expected) { user_notes.sort_by(&:id).reverse }
+
+      before { get :index }
+
+      it 'responds with the expected notes json' do
+        expect(response_body.to_json).to eq(expected)
+      end
+
+      it 'responds with 200 status' do
+        expect(response).to have_http_status(:ok)
+      end
+    end
+
+    context 'when fetching notes with page and page size params' do
+      let(:page)            { 1 }
+      let(:page_size)       { 2 }
+      let(:notes_expected) { user_notes.sort_by(&:id).reverse.first 2 }
+
+      before { get :index, params: { page: page, page_size: page_size } }
+
+      it 'responds with the expected notes' do
+        expect(response_body.to_json).to eq(expected)
+      end
+
+      it 'responds with 200 status' do
+        expect(response).to have_http_status(:ok)
+      end
+    end
+
+    context 'when fetching notes using filters' do
+      let(:type) { 'review' }
+
+      let!(:notes_custom) { create_list(:note, 2, type: type, content: 'words', user: user) }
+      let(:notes_expected) { notes_custom.sort_by(&:id).reverse }
+
+      before { get :index, params: { type: type } }
+
+      it 'responds with expected notes' do
+        expect(response_body.to_json).to eq(expected)
+      end
+
+      it 'responds with 200 status' do
+        expect(response).to have_http_status(:ok)
+      end
+    end
+  end
+end
