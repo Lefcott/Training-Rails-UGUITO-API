c0e34b7a8b4d049137b6a19ef4036a62932ad902
Leandro Cotti <leandro.cotti@widergy.com>
2024-02-27 16:04:49 -0300

    feat: separate content definition in tests

--- spec/models/note_spec.rb
+++ spec/models/note_spec.rb
@@ -25,78 +25,89 @@ RSpec.describe Note, type: :model do
 
   describe '#content_length' do
     context 'with NorthUtility' do
-      subject(:note) { create(:note, utility: north_utility) }
+      subject(:note) { create(:note, type: :critique, utility: north_utility, content: content) }
 
       context 'when content has less than 50 words' do
+        let(:content) { 'word ' * 49 }
+
         it 'returns short' do
-          subject.content = 'word ' * 49
           expect(subject.content_length).to eq 'short'
         end
       end
 
       context 'when content has 50 words' do
+        let(:content) { 'word ' * 50 }
+
         it 'returns short' do
-          subject.content = 'word ' * 50
           expect(subject.content_length).to eq 'short'
         end
       end
 
       context 'when content has less than 100 words' do
+        let(:content) { 'word ' * 99 }
+
         it 'returns medium' do
-          subject.content = 'word ' * 99
           expect(subject.content_length).to eq 'medium'
         end
       end
 
       context 'when content has 100 words' do
+        let(:content) { 'word ' * 100 }
+
         it 'returns medium' do
-          subject.content = 'word ' * 100
           expect(subject.content_length).to eq 'medium'
         end
       end
 
       context 'when content has more than 100 words' do
+        let(:content) { 'word ' * 101 }
+
         it 'returns long' do
-          subject.content = 'word ' * 101
           expect(subject.content_length).to eq 'long'
         end
       end
     end
 
     context 'with SouthUtility' do
-      subject(:note) { create(:note, utility: south_utility) }
+      subject(:note) { create(:note, type: :critique, utility: south_utility, content: content) }
 
       context 'when content has less than 60 words' do
+        let(:content) { 'word ' * 59 }
+
         it 'returns short' do
-          subject.content = 'word ' * 59
           expect(subject.content_length).to eq 'short'
         end
       end
 
       context 'when content has 60 words' do
+        let(:content) { 'word ' * 60 }
+
         it 'returns short' do
           subject.content = 'word ' * 60
           expect(subject.content_length).to eq 'short'
         end
       end
 
-      context 'when content has less than 100 words' do
+      context 'when content has less than 120 words' do
+        let(:content) { 'word ' * 119 }
+
         it 'returns medium' do
-          subject.content = 'word ' * 119
           expect(subject.content_length).to eq 'medium'
         end
       end
 
       context 'when content has 120 words' do
+        let(:content) { 'word ' * 120 }
+
         it 'returns medium' do
-          subject.content = 'word ' * 120
           expect(subject.content_length).to eq 'medium'
         end
       end
 
       context 'when content has more than 120 words' do
+        let(:content) { 'word ' * 121 }
+
         it 'returns long' do
-          subject.content = 'word ' * 121
           expect(subject.content_length).to eq 'long'
         end
       end
