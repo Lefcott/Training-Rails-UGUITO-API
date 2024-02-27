fe184b2a0fb2b0ef5ec434eca62db805ea4845bb
Leandro Cotti <leandro.cotti@widergy.com>
2024-02-27 11:26:32 -0300

    feat: added test for large page size

--- spec/controllers/api/v1/notes_controller_spec.rb
+++ spec/controllers/api/v1/notes_controller_spec.rb
@@ -40,6 +40,23 @@ describe Api::V1::NotesController, type: :controller do
       end
     end
 
+    context 'when page_size is too long' do
+      let(:page) { 1 }
+      let(:page_size) { 2000 }
+      let(:notes_expected) { user_notes.sort_by(&:id).reverse }
+      let(:expected_error) { 'page_size is too long, max allowed is 100' }
+
+      before { get :index, params: { page: page, page_size: page_size } }
+
+      it 'responds with 400 status' do
+        expect(response).to have_http_status(:bad_request)
+      end
+
+      it 'responds with the expected error' do
+        expect(response_body['error']).to eq(expected_error)
+      end
+    end
+
     context 'when fetching notes using filters' do
       let(:type) { 'review' }
       let!(:notes_custom) { create_list(:note, 2, type: type, content: 'words', user: user) }
