7309fe06e5eec6a3ce9ea3fe59eba0eb94c25187
Leandro Cotti <leandro.cotti@widergy.com>
2024-02-27 12:15:16 -0300

    feat: authenticate user in tests

--- spec/controllers/api/v1/notes_controller_spec.rb
+++ spec/controllers/api/v1/notes_controller_spec.rb
@@ -5,71 +5,75 @@ describe Api::V1::NotesController, type: :controller do
     let(:user) { create(:user) }
     let(:user_notes) { create_list(:note, 5, user: user) }
 
-    let!(:expected) do
-      ActiveModel::Serializer::CollectionSerializer.new(notes_expected,
-                                                        serializer: IndexNoteSerializer).to_json
-    end
+    context 'when there is a user logged in' do
+      include_context 'with authenticated user'
 
-    context 'when fetching all the notes for user' do
-      let(:notes_expected) { user_notes.sort_by(&:id).reverse }
-
-      before { get :index }
-
-      it 'responds with the expected notes json' do
-        expect(response_body.to_json).to eq(expected)
+      let!(:expected) do
+        ActiveModel::Serializer::CollectionSerializer.new(notes_expected,
+                                                          serializer: IndexNoteSerializer).to_json
       end
 
-      it 'responds with 200 status' do
-        expect(response).to have_http_status(:ok)
-      end
-    end
+      context 'when fetching all the notes for user' do
+        let(:notes_expected) { user_notes.sort_by(&:id).reverse }
 
-    context 'when fetching notes with page and page size params' do
-      let(:page) { 1 }
-      let(:page_size) { 2 }
-      let(:notes_expected) { user_notes.sort_by(&:id).reverse.first 2 }
+        before { get :index }
 
-      before { get :index, params: { page: page, page_size: page_size } }
+        it 'responds with the expected notes json' do
+          expect(response_body.to_json).to eq(expected)
+        end
 
-      it 'responds with the expected notes' do
-        expect(response_body.to_json).to eq(expected)
+        it 'responds with 200 status' do
+          expect(response).to have_http_status(:ok)
+        end
       end
 
-      it 'responds with 200 status' do
-        expect(response).to have_http_status(:ok)
-      end
-    end
+      context 'when fetching notes with page and page size params' do
+        let(:page) { 1 }
+        let(:page_size) { 2 }
+        let(:notes_expected) { user_notes.sort_by(&:id).reverse.first 2 }
 
-    context 'when page_size is too long' do
-      let(:page) { 1 }
-      let(:page_size) { 2000 }
-      let(:notes_expected) { user_notes.sort_by(&:id).reverse }
-      let(:expected_error) { 'page_size is too long, max allowed is 100' }
+        before { get :index, params: { page: page, page_size: page_size } }
 
-      before { get :index, params: { page: page, page_size: page_size } }
+        it 'responds with the expected notes' do
+          expect(response_body.to_json).to eq(expected)
+        end
 
-      it 'responds with 400 status' do
-        expect(response).to have_http_status(:bad_request)
-      end
-
-      it 'responds with the expected error' do
-        expect(response_body['error']).to eq(expected_error)
-      end
-    end
-
-    context 'when fetching notes using filters' do
-      let(:type) { 'review' }
-      let!(:notes_custom) { create_list(:note, 2, type: type, content: 'words', user: user) }
-      let(:notes_expected) { notes_custom.sort_by(&:id).reverse }
-
-      before { get :index, params: { type: type } }
-
-      it 'responds with expected notes' do
-        expect(response_body.to_json).to eq(expected)
+        it 'responds with 200 status' do
+          expect(response).to have_http_status(:ok)
+        end
       end
 
-      it 'responds with 200 status' do
-        expect(response).to have_http_status(:ok)
+      context 'when page_size is too long' do
+        let(:page) { 1 }
+        let(:page_size) { 2000 }
+        let(:notes_expected) { user_notes.sort_by(&:id).reverse }
+        let(:expected_error) { 'page_size is too long, max allowed is 100' }
+
+        before { get :index, params: { page: page, page_size: page_size } }
+
+        it 'responds with 400 status' do
+          expect(response).to have_http_status(:bad_request)
+        end
+
+        it 'responds with the expected error' do
+          expect(response_body['error']).to eq(expected_error)
+        end
+      end
+
+      context 'when fetching notes using filters' do
+        let(:type) { 'review' }
+        let!(:notes_custom) { create_list(:note, 2, type: type, content: 'words', user: user) }
+        let(:notes_expected) { notes_custom.sort_by(&:id).reverse }
+
+        before { get :index, params: { type: type } }
+
+        it 'responds with expected notes' do
+          expect(response_body.to_json).to eq(expected)
+        end
+
+        it 'responds with 200 status' do
+          expect(response).to have_http_status(:ok)
+        end
       end
     end
   end
@@ -78,25 +82,30 @@ describe Api::V1::NotesController, type: :controller do
     let(:expected) { ShowNoteSerializer.new(note, root: false).to_json }
     let(:user) { create(:user) }
 
-    context 'when fetching a valid note' do
-      let(:note) { create(:note, user: user) }
 
-      before { get :show, params: { id: note.id } }
+    context 'when there is a user logged in' do
+      include_context 'with authenticated user'
 
-      it 'responds with the note json' do
-        expect(response.body).to eq(expected)
+      context 'when fetching a valid note' do
+        let(:note) { create(:note, user: user) }
+
+        before { get :show, params: { id: note.id } }
+
+        it 'responds with the note json' do
+          expect(response.body).to eq(expected)
+        end
+
+        it 'responds with 200 status' do
+          expect(response).to have_http_status(:ok)
+        end
       end
 
-      it 'responds with 200 status' do
-        expect(response).to have_http_status(:ok)
-      end
-    end
+      context 'when fetching an invalid note' do
+        before { get :show, params: { id: Faker::Number.number } }
 
-    context 'when fetching an invalid note' do
-      before { get :show, params: { id: Faker::Number.number } }
-
-      it 'responds with 404 status' do
-        expect(response).to have_http_status(:not_found)
+        it 'responds with 404 status' do
+          expect(response).to have_http_status(:not_found)
+        end
       end
     end
   end
